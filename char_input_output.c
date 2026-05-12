a4e34b8301cb8f06923b9d6e18319f0fb77c9539
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:54:35 +0530

    feat: Add char input output experiment

---
+++ char_input_output.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char x[30], n[30], c[30];
+    
+    printf("Enter the first character sequence: ");
+    scanf("%29s", x);
+    printf("You entered: %s\n", x);
+
+    printf("Enter the second character sequence: ");
+    scanf("%29s", n);
+    printf("You entered: %s\n", n);
+
+    printf("Enter the third character sequence: ");
+    scanf("%29s", c);
+    printf("You entered: %s\n", c);
+
+    return 0;
+}
