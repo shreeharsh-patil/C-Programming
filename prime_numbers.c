9e1eb43ff092211288e8dbdb09e8dfaa91b4921a
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:55:20 +0530

    feat: Add prime numbers experiment

---
+++ prime_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1, i, count;
+    printf("Listing prime numbers up to 100:\n");
+    while (a < 100)
+    {
+        count = 0;
+        i = 2;
+        while (i <= a / 2)
+        {
+            if (a % i == 0)
+            {
+                count++;
+                break;
+            }
+            i++;
+        }
+        if (count == 0 && a != 1)
+        {
+            printf("%d ", a);
+        }
+        a++;
+    }
+    printf("\n");
+    return 0;
+}
