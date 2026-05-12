6909a2fc6b71eb9c1e02a76e929cd662a4609104
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:55:32 +0530

    feat: Add relational operators experiment

---
+++ relational_operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 60, y = 30;
+    printf("Relational Operators testing between %d and %d:\n", x, y);
+    printf("60 == 30 -> %d\n", x == y);
+    printf("60 != 30 -> %d\n", x != y);
+    printf("60 >= 30 -> %d\n", x >= y);
+    printf("60 <= 30 -> %d\n", x <= y);
+    printf("60 < 30  -> %d\n", x < y);
+    printf("60 > 30  -> %d\n", x > y);
+    return 0;
+}
