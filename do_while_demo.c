7889a83ecfdd3edaa096b3cae23e3896a201ee31
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:54:48 +0530

    feat: Add do while demo experiment

---
+++ do_while_demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    printf("Example of a do-while loop:\n");
+    do {
+        printf("Iteration %d\n", i);
+        i++;
+    } while (i <= 5);
+    return 0;
+}
